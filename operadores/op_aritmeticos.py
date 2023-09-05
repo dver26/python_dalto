b5682c2c616e9da0f2531fe4efa2d461ef8cbe67
dver26 <fercorvictor@gmail.com>
2023-09-05 23:24:44 +0200

    operadores añadidos

---
+++ operadores/op_aritmeticos.py
@@ -0,0 +1,16 @@
+# suma y resta
+suma = 12 + 5
+resta = 12 - 5
+
+# multi y division
+multi = 12 * 5
+division = 12 / 5 # devuelve un dato flotante igualmente que resulte un número entero en la operación
+
+# potencia
+exponente = 12**5
+
+# división baja
+division_baja = 12//5 # redondea para dar como resultado el número entero más bajo
+
+print(exponente)
+print(type("Hola"))
